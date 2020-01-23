5e13d8757e61802948e89067e4e0541bc1fd48e4
Zachary Miller <zmiller@gohealth.com>
2020-01-23 16:16:08 -0600

    First commit

---
+++ src/main/java/com/miller/finance/miller/finance/MillerFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.miller.finance.miller.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillerFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillerFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miller/finance/miller/finance/MillerFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miller.finance.miller.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillerFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
